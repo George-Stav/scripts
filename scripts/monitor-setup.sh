cb21f79c39e8d77f0bb2fd10b518a2994a90dac5
George Stavropoulos <georgestavropoulos0@gmail.com>
2022-01-29 12:35:57 +0000

    monitor-setup

--- scripts/monitor-setup.sh
+++ scripts/monitor-setup.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
-xrandr --rate 144
+set -euo pipefail
 
-xrandr --output "HDMI-1-0" --mode 1920x1080 --rate 144 --right-of "DP-1"
+internal="eDP-1"
+external1="DP-1"
+external2="HDMI-1-0"
+
+if [ $(xrandr | grep "$external1" | awk '{print $2}' | sed -n '2 p') == 'connected' ]; then
+    xrandr --output $internal --off
+    xrandr --rate 144
+    xrandr --output $external1 --primary --mode 1920x1080 --rate 144
+    [[ $(xrandr | grep "$external2" | awk '{print $2}') == 'connected' ]] && \
+        xrandr --output $external2 --mode 1920x1080 --rate 144 --left-of $external1
+elif [[ $(xrandr | grep "$external2" | awk '{print $2}') == 'connected' ]]; then
+    # xrandr --output $internal --off
+    # xrandr --rate 144
+    xrandr --output $external2 --primary --mode 1920x1080
+fi
